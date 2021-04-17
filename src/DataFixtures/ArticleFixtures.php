b409a80af0b5083290a6a35ff12d7513cefbe44f
tony dugué <duguetony@gmail.com>
2021-04-17 15:29:29 +0200

    modificatif: fixtures des articles

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -14,18 +14,49 @@ class ArticleFixtures extends Fixture
         // Initialisation du générateur Faker
         $faker = Factory::create('fr_FR');
 
-        for($i = 1; $i <= 10; $i++) {
-            $article = new Article();
+        $article1 = new Article();
 
-            $article
-                ->setTitle($faker->sentence(5, true))
-                ->setPicture('')
-                ->setExcerpt($faker->text(150))
-                ->setContent($faker->paragraph(30, true));
+        $article1
+            ->setTitle("Comment planifier le meilleur itinéraire ?")
+            ->setPicture("Au moment de prendre la route lors d'une balade en moto, la question 
+            du chemin à prendre survient. Pour rouler en toute sérénité et choisir le trajet qui vous 
+            convient le mieux, il est conseillé de se renseigner en amont. Mais que vérifier et quels 
+            moyens utiliser pour trouver le meilleur chemin ?")
+            ->setExcerpt("")
+            ->setContent("$faker->paragraph(30, true)");
 
-            $manager->persist($article);
-        }
+        $manager->persist($article1);
 
-        $manager->flush();
+        $article2 = new Article();
+
+        $article2
+            ->setTitle("Les meilleurs applications météo")
+            ->setPicture("Vous êtes à la recherche de la meilleure application météo pour votre 
+            balade en moto ? Trouvez ici la liste des meilleures applications disponibles.")
+            ->setExcerpt("")
+            ->setContent("$faker->paragraph(30, true)");
+
+        $manager->persist($article2);
+
+        $article3 = new Article();
+        $article3
+            ->setTitle("Top 10 des points remarquables autour de Saint Malo")
+            ->setPicture("Retrouver le top 10 des plus beaux points remarquables et des idées de 
+            balade autour de Saint-Malo.")
+            ->setExcerpt("")
+            ->setContent("$faker->paragraph(30, true)");
+
+        $manager->persist($article3);
+
+        $article4 = new Article();
+
+        $article4
+            ->setTitle("Liste des choses à ne pas oublier pendant un roadtrip")
+            ->setPicture("Retrouver quelques conseils pour ne rien oublier et parer aux petits aléas 
+            sur la route. Liste des 10 choses à savoir si vous partez en roadtrip.")
+            ->setExcerpt("")
+            ->setContent("$faker->paragraph(30, true)");
+
+        $manager->persist($article4);
     }
 }
